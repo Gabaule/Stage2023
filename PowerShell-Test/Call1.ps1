9752215ff3042f535a71902f1d263309e435ac84
Gabriel Aumasson--Leduc <gabaulepro@gmail.com>
2023-05-15 11:23:36 +0200

    Ecriture de premiers script en PowerShell

---
+++ PowerShell-Test/Call1.ps1
@@ -0,0 +1,1 @@
+Write-Output "On est dans le script 1"
---
+++ PowerShell-Test/General.ps1
@@ -0,0 +1,5 @@
+Write-Output "On est dans le script général"
+Start-Sleep 3
+Invoke-Expression -Command "C:\Users\Gabriel\Desktop\Stage2023\PowerShell-Test\Call1.ps1"
+Start-Sleep 3
+Invoke-Expression -Command "C:\Users\Gabriel\Desktop\Stage2023\PowerShell-Test\Call2.ps1"
